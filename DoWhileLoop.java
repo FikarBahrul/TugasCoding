2bdb02f063f08b6239acce32cb21e9c6112a04a9
unknown <fikarbahrul99@gmail.com>
2024-09-23 12:58:53 +0700

    DoWhileLoop

---
+++ DoWhileLoop.java
@@ -0,0 +1,13 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        
+        var counter = 1;
+
+        do {
+            System.out.println("Dimas"+counter);
+            counter++;
+        } while (counter <=5);
+
+    }
+    
+}
