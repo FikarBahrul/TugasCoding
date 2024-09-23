e3072d7a0bc85c01bde4e929d858109a6dbde16a
unknown <fikarbahrul99@gmail.com>
2024-09-23 12:36:45 +0700

    Baru

---
+++ PerulanganLoop.java
@@ -0,0 +1,9 @@
+public class PerulanganLoop {
+    public static void main(String[] args) {
+        
+        var counter = 1;
+
+        for(; counter <= 5 ; counter++) {
+            System.out.println("Dimas "+counter );}
+    }
+}
